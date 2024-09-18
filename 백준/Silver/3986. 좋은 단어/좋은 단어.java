8d8e9741db699aaf0b4e33d8920bc51931305149
syeongk <syeongk7@gmail.com>
2024-09-18 19:14:05 +0900

    [Silver IV] Title: 좋은 단어, Time: 308 ms, Memory: 22676 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,40 @@
+
+
+import java.util.Stack;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i=0; i<n; i++){
+            Stack<Character> stack = new Stack<>();
+            String S = br.readLine();
+            Character str;
+
+            for (int j=0; j<S.length(); j++){
+
+                // 스택이 비어있거나 top과 문자가 다를 때
+                if (stack.isEmpty() || stack.peek() != S.charAt(j)){
+                    stack.push(S.charAt(j));
+                }
+                 // 스택 top과 문자가 같을 때
+                else if (stack.peek() == S.charAt(j)){
+                    str = stack.pop();
+                }
+            }
+
+            if(stack.isEmpty() ){
+                result += 1;
+            }
+
+
+        }
+        System.out.println(result);
+    }
+}
