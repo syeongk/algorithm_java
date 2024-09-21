cf6e2313228ca35fefb452d504c073c0f42cc660
syeongk <syeongk7@gmail.com>
2024-09-21 17:21:24 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 228 ms, Memory: 16852 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,40 @@
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String[] input = str.split(" ");
+        Deque<Integer> q = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+
+
+        for (int i=1; i<n+1; i++){
+            q.offer(i);
+        }
+
+        sb.append('<');
+        while (!q.isEmpty()){
+            for (int i=0; i<k-1; i++){
+                q.offer(q.poll());
+            }
+            if (q.size()==1){
+                sb.append(q.poll());
+            } else {
+                sb.append(q.poll()).append(',').append(" ");
+            }
+        }
+        sb.append('>');
+        System.out.println(sb);
+    }
+}
