92f287886703b022c074b98211d090b93bf29496
syeongk <syeongk7@gmail.com>
2024-09-25 20:03:51 +0900

    [Silver III] Title: 크리스마스 선물, Time: 280 ms, Memory: 35588 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -9,29 +9,30 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
+        StringTokenizer st = null;
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        for (int i=0; i<N ; i++){
-
-            String[] input = br.readLine().split(" ");
-
-            if (input.length==1) {
+        for (int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            if (a==0){
                 if(pq.isEmpty()){
                     sb.append(-1).append("\n");
                 } else {
                     sb.append(pq.poll()).append("\n");
                 }
-            }
-
-            for (int j = 0; j< Integer.parseInt(input[0]) ; j++){
-                pq.offer(Integer.parseInt(input[j+1]));
+            } else {
+                for (int j=0; j<a; j++){
+                    int present = Integer.parseInt(st.nextToken());
+                    pq.offer(present);
+                }
             }
         }
         System.out.println(sb);
     }
+
 }
+
