36a58e6929465a43fa30e0a2e0713c8209a71d8a
syeongk <syeongk7@gmail.com>
2024-10-01 07:53:06 +0900

    [Silver II] Title: 생태학, Time: 824 ms, Memory: 98636 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.TreeMap;
 
 public class Main {
@@ -11,13 +10,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         TreeMap<String, Double> hm = new TreeMap<>();
 
-
+        int total = 0;
         while(true){
             String str = br.readLine();
-
             if (str==null || str.equals("")){
                 break;
             }
+            total += 1;
             if (hm.containsKey(str)){
                 double cnt = hm.get(str);
                 hm.put(str, cnt+1);
@@ -26,16 +25,11 @@ public class Main {
             }
         }
 
-        int sum = 0;
-        for (String key : hm.keySet()){
-            sum += hm.get(key);
-        }
-
         StringBuilder sb = new StringBuilder();
         for (String key : hm.keySet()){
             double value = hm.get(key);
 
-            String value2 = String.format("%.4f", (value*100) / sum);
+            String value2 = String.format("%.4f", (value*100) / total);
             hm.put(key, Double.parseDouble(value2));
             sb.append(key).append(" ").append(value2).append('\n');
         }
