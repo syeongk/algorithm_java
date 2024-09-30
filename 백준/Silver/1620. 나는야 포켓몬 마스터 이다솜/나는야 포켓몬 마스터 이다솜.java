b0ffebd3b2a948a4df70e3f65e1698314bc45199
syeongk <syeongk7@gmail.com>
2024-10-01 05:08:19 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 428 ms, Memory: 64840 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -7,8 +7,8 @@ public class Main {
 
     public void solution() throws IOException {
 
-        HashMap<String, String> name = new HashMap<>();
-        HashMap<String, String> num = new HashMap<>();
+        HashMap<String, Integer> name = new HashMap<>();
+        HashMap<Integer, String> num = new HashMap<>();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
@@ -19,16 +19,16 @@ public class Main {
 
         for (int i=1; i<n+1; i++){
             String poketmon = br.readLine();
-            name.put(poketmon, Integer.toString(i));
-            num.put(Integer.toString(i), poketmon);
+            name.put(poketmon, i);
+            num.put(i, poketmon);
         }
 
         for (int i=0; i<m; i++){
             String q = br.readLine();
             if(name.containsKey(q)){
                 sb.append(name.get(q)).append("\n");
-            } else if(num.containsKey(q)) {
-                sb.append(num.get(q)).append("\n");
+            } else if(num.containsKey(Integer.parseInt(q))) {
+                sb.append(num.get(Integer.parseInt(q))).append("\n");
             }
         }
 
