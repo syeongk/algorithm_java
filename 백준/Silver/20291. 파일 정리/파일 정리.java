c3dc0d690e0aefe4344eac89196e3a3e0b88aef9
syeongk <syeongk7@gmail.com>
2024-10-02 19:03:06 +0900

    [Silver III] Title: 파일 정리, Time: 516 ms, Memory: 79852 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        TreeMap<String, Integer> tm = new TreeMap<>();
+        for (int i=0; i<N ; i++){
+            st = new StringTokenizer(br.readLine(), ".");
+            st.nextToken();
+            String name = st.nextToken();
+            if (tm.containsKey(name)){
+                int value = tm.get(name);
+                tm.put(name, value+1);
+            } else {
+                tm.put(name, 1);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String key : tm.keySet()){
+            int count = tm.get(key);
+            sb.append(key).append(" ").append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
