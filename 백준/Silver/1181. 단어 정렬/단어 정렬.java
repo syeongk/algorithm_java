1c1a757f3383fb1c96a47cac36bb03799a8e846a
syeongk <syeongk7@gmail.com>
2024-10-20 20:51:06 +0900

    [Silver V] Title: 단어 정렬, Time: 332 ms, Memory: 42996 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,40 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.TreeSet;
 
 public class Main {
 
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        HashSet<String> set = new HashSet<>();
-        List<String> li = new LinkedList<>();
+        TreeSet<String> set = new TreeSet<>((a,b)->{
+            if(a.length()!=b.length())
+                return a.length()-b.length();
+            else
+                return a.compareTo(b);
+        });
+
         for (int i=0; i<n; i++){
             set.add(br.readLine());
         }
-        for (String s : set){
-            li.add(s);
-        }
 
-        Collections.sort(li, new Comparator<String>(){
-            @Override
-            public int compare(String o1, String o2){
-                int s1 = o1.length();
-                int s2 = o2.length();
-                if (s1>s2) return 1;
-                else if(s1<s2) return -1;
-                else{
-                    int headValue = o1.compareTo(o2);
-                    return headValue;
-                }
-            }
-        });
-
-        for(String word : li){
-            System.out.println(word);
+        for(String word : set){
+            sb.append(word+"\n");
         }
+        System.out.println(sb);
     }
 
     public static void main(String[] args) throws IOException {
