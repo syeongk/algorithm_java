95a270cf4d51cf802be51d7a5103374feb264975
syeongk <syeongk7@gmail.com>
2024-11-22 21:22:39 +0900

    [Silver V] Title: 거스름돈, Time: 92 ms, Memory: 12860 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        new Main().solution();
+    }
+
+    public void solution() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int five = n / 5; // 5의 최대 개수
+        int two = 0;
+
+        while (true) {
+            if (five == 0){
+                if ((n % 2) != 0) {
+                    break;
+                }
+                two = n / 2;
+            }
+
+            if ((n - 5 * five) % 2 != 0) { //5로 나누었을 때 나머지가 2로 나누어 떨어지지 않으면
+                five -= 1; // 5의 최대 개수 - 1
+            } else {
+                two = (n - 5 * five) / 2; //5로 나누었을 때 나머지가 2로 나누어 떨어지면, 나머지를 2로 나눈 몫이 2의 개수
+                break;
+            }
+        }
+
+        if (two + five == 0){
+            System.out.println(-1);
+        } else {
+            System.out.println(two + five);
+        }
+
+
+    }
+}
