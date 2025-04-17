36972c9c1ba0fe52d9c1372962fdbce50c700179
syeongk <syeongk7@gmail.com>
2025-04-17 16:26:21 +0900

    [Silver IV] Title: 카드2, Time: 108 ms, Memory: 23720 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,32 +1,30 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
+//1234
+//234 (1번)
+//342 (2번)
+//42 (1번)
+//24 (2번)
+//4 (1번)
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n*2];
+        Queue<Integer> q = new ArrayDeque<>();
 
-        int front = 0;
-        int back = n-1;
-
-        for (int i=0; i<n; i++){
-            arr[i] = i+1;
+        for (int i=1; i<=N; i++){
+            q.offer(i);
         }
 
-        while (back!=front) {
-            front++;
-            arr[back+1] = arr[front];
-            back += 1;
-            front += 1;
+        while (q.size() > 1){
+            q.poll();
+            q.offer(q.poll());
         }
-
-        System.out.println(arr[back]);
-
+        System.out.println(q.poll());
     }
 }
